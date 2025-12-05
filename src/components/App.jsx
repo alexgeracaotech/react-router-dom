91d23ba15768ec45759b600c85d620d6a00a66a9
Alex Bessa <alexbessa.dev@gmail.com>
2025-12-04 21:00:46 -0300

    finish routes

---
+++ src/components/App.jsx
@@ -0,0 +1,45 @@
+
+import './App.css';
+import { Link } from 'react-router-dom';
+import AppRouter from '../routes.jsx';
+
+const App = () => {
+
+  // let page;
+  // const pathname = window.location.pathname;
+
+  // switch (pathname) {
+  //   case '/':
+  //     page = <Home />;
+  //     break;
+  //   case '/products':
+  //     page = <Products />;
+  //     break;
+  //   case '/about':
+  //     page = <About />;
+  //     break;
+  //   default:
+  //     page = <NotFound />;
+  // }
+
+  return (
+    <>
+      <header>
+        <Link to='/'>Logo</Link>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/products'>Products</Link>
+            </li>
+            <li>
+              <Link to='/about'>About</Link> 
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <AppRouter />
+    </>
+  );
+}
+
+export default App;
